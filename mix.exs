399c6ede01765fcbdc43c4cf7f0942e48d813121
Jos van Bakel <jos@codeaddict.org>
2020-07-18 09:28:02 +0200

    Fix and update

--- mix.exs
+++ mix.exs
@@ -2,31 +2,48 @@ defmodule NifIo.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :nif_io,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     compilers: [:rustler] ++ Mix.compilers,
-     rustler_crates: rustler_crates(),
-     deps: deps()]
+    [
+      app: :nif_io,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      aliases: aliases(),
+      compilers: [:rustler] ++ Mix.compilers(),
+      rustler_crates: rustler_crates()
+    ]
   end
 
   def application do
-    [applications: [:logger]]
+    [
+      extra_applications: [:logger]
+    ]
   end
 
   defp deps do
-    [{:rustler, github: "hansihe/rustler", sparse: "rustler_mix"}]
+    [
+      {:rustler, "~> 0.22-rc"}
+    ]
   end
 
   defp rustler_crates do
-    [io: [
-      path: "native/io",
-      mode: rustc_mode(Mix.env)
-    ]]
+    [
+      io: [
+        path: "native/io",
+        mode: rustc_mode(Mix.env())
+      ]
+    ]
   end
 
   defp rustc_mode(:prod), do: :release
   defp rustc_mode(_), do: :debug
+
+  defp aliases do
+    [
+      fmt: [
+        "format",
+        "cmd cargo fmt --manifest-path native/io/Cargo.toml"
+      ]
+    ]
+  end
 end
