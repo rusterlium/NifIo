760c6bde18697c88536ae7b5ba9e7e9321dca32c
Philip Sampaio <philip.sampaio@gmail.com>
2022-03-16 18:03:31 -0300

    Add CI workflow
    Also bump minimum Elixir version to 1.11, like Rustler.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NifIo.Mixfile do
     [
       app: :nif_io,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
